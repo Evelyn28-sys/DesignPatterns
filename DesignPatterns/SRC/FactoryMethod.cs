5fec4b92c86197cae306712e6e9fdf314e8bea85
Evelyn Araujo <Evelyn.Araujo@gep.com>
2026-02-02 13:22:49 -0300

    ajuste no catalogo

--- DesignPatterns/SRC/FactoryMethod.cs
+++ DesignPatterns/SRC/FactoryMethod.cs
@@ -33,13 +33,50 @@
         public override Payment CreatePayment() => new BoletoPayment();
     }
 
+    public abstract class Product
+    {
+        public abstract string GetInfo();
+    }
+    public class Batom : Product
+    {
+        public override string GetInfo() => "Produto: Batom - Cor vibrante e longa duração";
+    }
+    public class Base : Product
+    {
+        public override string GetInfo() => "Produto: Base - Cobertura perfeita para sua pele";
+    }
+    public class Mascara : Product
+    {
+        public override string GetInfo() => "Produto: Máscara de cílios - Volume e definição";
+    }
+    public abstract class ProductFactory
+    {
+        public abstract Product CreateProduct();
+    }
+    public class BatomFactory : ProductFactory
+    {
+        public override Product CreateProduct() => new Batom();
+    }
+    public class BaseFactory : ProductFactory
+    {
+        public override Product CreateProduct() => new Base();
+    }
+    public class MascaraFactory : ProductFactory
+    {
+        public override Product CreateProduct() => new Mascara();
+    }
+
     public static class FactoryMethodDemo
     {
         public static void Run()
         {
-            PaymentFactory factory = new PixPaymentFactory();
-            var payment = factory.CreatePayment();
+            PaymentFactory paymentFactory = new PixPaymentFactory();
+            var payment = paymentFactory.CreatePayment();
             Console.WriteLine(payment.Pay(100));
+
+            ProductFactory productFactory = new BatomFactory();
+            var product = productFactory.CreateProduct();
+            Console.WriteLine(product.GetInfo());
         }
     }
 }
